22fca908a9306dc7cf496c42b486fef39f89bb62
Code with Ankush <ankush@codewithankush.com>
2025-05-26 18:13:16 +0530

    The stats cards are functional showing the pending order and revenue

--- src/main/java/com/laundry/repo/OrderRepository.java
+++ src/main/java/com/laundry/repo/OrderRepository.java
@@ -2,26 +2,24 @@ package com.laundry.repo;
 
 import com.laundry.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByCustomerId(Long customerId);
 
-//    Order findById();
-
-
     List<Order> findAllByOrderByOrderDateDesc();
 
     List<Order> findByStatus(Order.OrderStatus status);
 
+    @Query("Select COUNT(o) from Order o where o.status='PENDING' ")
+    long countByStatus(Order.OrderStatus status);
 
-//    List<Order> findByStatus(OrderStatus status); // For filtering
-//    long countByStatus(OrderStatus status); // For dashboard stats
-//List<Order> findByStatus(Order.OrderStatus status);
-
-
-    // For reports
+    @Query("Select COALESCE(sum(o.totalAmount),0) from Order o where o.orderDate= :date")
+    double sumOfRevenueToday(@Param("date")LocalDate date);
 //    @Query("SELECT o FROM Order o WHERE o.orderDate BETWEEN :start AND :end")
 //    List<Order> findOrdersBetweenDates(@Param("start") LocalDate start,
 //                                       @Param("end") LocalDate end);
