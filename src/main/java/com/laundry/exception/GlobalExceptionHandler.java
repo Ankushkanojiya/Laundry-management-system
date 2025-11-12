fcdf830d106e722c5eb104d948e9c53585304899
Code with Ankush <43909258+Ankushkanojiya@users.noreply.github.com>
2025-11-12 20:52:01 +0530

    Added exception methods to handle Runtime And Validation Execeptions

--- src/main/java/com/laundry/exception/GlobalExceptionHandler.java
+++ src/main/java/com/laundry/exception/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.laundry.exception;
 
 import com.laundry.dto.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -22,5 +25,35 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorResponse,exce.getHttpStatus());
     }
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponseDTO> handleGenericRuntimeException(RuntimeException exce) {
+        ErrorResponseDTO errorResponse = ErrorResponseDTO.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value()) // 400
+                .error(exce.getMessage())
+                .build();
 
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+    /**
+     * Catches DTO validation errors (like @NotBlank, @Size)
+     * and returns a 400 Bad Request with a cleaner message.
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDTO> handleValidationExceptions(MethodArgumentNotValidException exce) {
+
+        String errorMessage = "Validation failed";
+        FieldError fieldError = exce.getBindingResult().getFieldError();
+        if (fieldError != null) {
+            errorMessage = fieldError.getDefaultMessage();
+        }
+
+        ErrorResponseDTO errorResponse = ErrorResponseDTO.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error(errorMessage)
+                .build();
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
